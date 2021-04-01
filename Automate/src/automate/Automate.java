23d6c4b6694f813be1af2e18670d21f96672dfe8
Antonin Lampin <antonin.lampin@efrei.net>
2021-04-01 18:45:40 +0200

    Lire_automate_fichier
    Ajout fonctionnalité lire dans un fichier un automate et le transcrire dans notre base de données

--- Automate/src/automate/Automate.java
+++ Automate/src/automate/Automate.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Iterator;
 
 public class Automate {
 	static public final int MAX = 26; // On acte qu'un etat ne peut pas avoir plus de 26 transition 
@@ -16,7 +15,6 @@ public class Automate {
 
 	private int[] etatInit;
 	private int[] etatTerm;
-	private int index;
 
 	// Constructeur par defaut
 	public Automate() {
@@ -27,4 +25,103 @@ public class Automate {
 		this.nbrEtats = nbrEtat;
 	}
 
+	
+	
+	
+	
+	public void lire_automate_fichier(String NomFichier) {
+
+		// ouverture du fichier
+		File fichier = new File(NomFichier);
+		// On regarde si le fichier n'existe pas
+		if (!fichier.exists()) {
+			System.out.println("Le fichier n'existe pas, veuillez réessayer !");	
+		} 
+		// Si il existe on peut travailler
+		else {
+			try {
+				BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(fichier), "UTF-8"));
+				
+				// Lecture de l'Alphabet
+				String Lines = reader.readLine();
+				alphabet = new char[Integer.parseInt(Lines)];
+				char lettre = 'a';
+				for (int i = 0; i < Integer.parseInt(Lines); i++) {
+					alphabet[i] = lettre++;
+					//System.out.println(alphabet[i]); // A SUPPRIMER
+				}
+				
+				// Nombre d'etats total et creation des etats
+				Lines = reader.readLine();
+				nbrEtats = Integer.parseInt(Lines);
+				etats = new Etat[nbrEtats];
+				
+				// Nombre d'etat initiaux
+				char nbr = (char) reader.read();
+				int nombreEtatInit = Character.getNumericValue(nbr);
+				etatInit = new int[nombreEtatInit];
+				for (int i = 0; i < nombreEtatInit; i++) {
+					nbr = (char) reader.read(); // A cause du caractere espace
+					nbr = (char) reader.read();
+					etatInit[i] = Character.getNumericValue(nbr);
+					//System.out.println("etat = " + etatInit[i] + " ");// A SUPPRIMER
+				} 
+				
+				// Nombre d'etat finaux
+				Lines = reader.readLine();
+				nbr = (char) reader.read();
+				nombreEtatInit = Character.getNumericValue(nbr);
+				etatTerm = new int[nombreEtatInit];
+				for (int i = 0; i < nombreEtatInit; i++) {
+					nbr = (char) reader.read(); // A cause du caractere espace
+					nbr = (char) reader.read();
+					etatTerm[i] = Character.getNumericValue(nbr);
+					//System.out.println("etat = " + etatTerm[i] + " ");// A SUPPRIMER
+				}
+				Lines = reader.readLine(); // On finit la ligne
+				
+				// Nombre de transition 
+				Lines = reader.readLine();
+				int nbrTransition = Integer.parseInt(Lines); 
+				
+				//Transition tabTransition = new Transition 
+				// Les transitions
+				Transition[] transEtati= new Transition[MAX]; 
+				int j = 0, etatActuelle = 0; 
+				int numeroEtatEntree = 0, numeroEtatSortie = 0; 
+				char translettre;
+				for (int i = 0; i < nbrTransition; i++) {
+					//Recuperation du l'etat de depart
+					nbr =(char) reader.read();
+					numeroEtatEntree = Character.getNumericValue(nbr);
+					//Recuperation de la lettre
+					nbr =(char) reader.read();
+					translettre = nbr; 
+					//Recuperation de l'etat final
+					nbr = (char) reader.read(); 
+					numeroEtatSortie = Character.getNumericValue(nbr); 
+
+					Transition t = new Transition(numeroEtatEntree, translettre, numeroEtatSortie);
+					// Si l'etat de depart change c'est que nous avons parcouru toutes les transitions
+					if (numeroEtatEntree != etatActuelle) { 
+						etats[etatActuelle] = new Etat(numeroEtatEntree-1, transEtati, j); 
+						etatActuelle++; // On passe a un etat N+1
+						j = 0;// On repart à la premier case du tableau 
+					}
+					
+					transEtati[j] = t.copie();					
+					j++;
+					//Fin de la ligne
+					Lines = reader.readLine(); 
+				}
+				etats[numeroEtatEntree] = new Etat(numeroEtatEntree, transEtati, j);
+				reader.close();
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 }
